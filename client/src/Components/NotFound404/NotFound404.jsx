a4ca4395249ddb2280c04204a29b9fad525399bf
siyaa12 <21bec108@iiitdmj.ac.in>
2022-10-31 19:47:31 +0530

    updated UI of 404NotFoundPage

--- client/src/Components/NotFound404/NotFound404.jsx
+++ client/src/Components/NotFound404/NotFound404.jsx
@@ -1,25 +1,46 @@
-import React from "react";
-import { Container } from "@mui/system";
-import { Button, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom'
+import { Box, Button, Container, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid';
 
 const NotFound404 = () => {
   return (
-    <Container>
-      <Typography sx={{ textAlign: "center", margin: "2rem" }} variant="h1">
-        404 Not Found
-      </Typography>
-      <Container sx={{ display: "flex", justifyContent: "center" }}>
-        <Button variant="contained">
-          <Link to="/">
-            <Typography sx={{ textDecoration: "none", color: "white" }}>
-              Search Another
+    <Box
+      sx={{
+        marginTop:"-7rem",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh'
+      }}
+    >
+      <Container maxWidth="md">
+        <Grid container spacing={2}>
+          <Grid xs={6}>
+            <Typography variant="h1">
+              404
             </Typography>
-          </Link>
-        </Button>
+            <Typography variant="h6">
+              The Profile you're looking for doesn't exist.
+            </Typography>
+            <Button variant="contained"><Link to="/">
+              <Typography sx={{ textDecoration: "none", color: "white" }}>
+                Search Another
+              </Typography>
+            </Link></Button>
+          </Grid>
+          <Grid xs={6}>
+            <img
+              src="https://cdn.pixabay.com/photo/2017/03/09/12/31/error-2129569__340.jpg"
+              alt=""
+              width={500} height={250}
+            />
+          </Grid>
+        </Grid>
       </Container>
-    </Container>
+    </Box>
   );
-};
+}
 
 export default NotFound404;
+
