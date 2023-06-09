499a5174faf473e1ebc44eb8f3e07e446b2d4724
janmesh kumar <janmesh799@gmail.com>
2023-06-09 11:11:49 +0530

    added comments to the code

--- server/Routes/auth/loginUser.js
+++ server/Routes/auth/loginUser.js
@@ -1,49 +1,58 @@
-const User = require('../../Models/User');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const User = require("../../Models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const secKey = process.env.SECKEY;
 
 const loginUser = async (req, res) => {
-    let errorCode = null;
-    try {
-        // extracting credentials from body of request
-        let { email, password } = req.body;
-        email = email.toLowerCase();
+  let errorCode = null;
+  try {
+    // extracting credentials from body of request
+    let { email, password } = req.body;
+    email = email.toLowerCase();
 
-        // finding if user exists
-        let user = await User.findOne({ email });
-        if (!user) {
-            errorCode = 403;
-            throw new Error("Please try to login with correct credentials")
-        }
-
-        // comparing passwords
-        const passwordCompare = await bcrypt.compare(password, user.password);
-        console.log(passwordCompare)
-        if (!passwordCompare) {
-            errorCode = 403;
-            throw new Error("Please try to login with correct credentials")
-        }
-
-        // if passwords get authenticated, creating auth token
-        const data = {
-            user: {
-                id: user.id,
-                username: user.username,
-                email: user.email,
-                name: user.name
-            },
-        }
-        const authToken = jwt.sign(data, secKey);
-
-        // sending response
-        res.json({ authToken, success: true, user: data.user, message: "Login Successful" });
-
-    } catch (err) {
-        
-        // sending response with errorCode 500 and error message if any error raised
-        return res.status(errorCode || 500).json({ success: false, message: "Internal server Error", error: err.message })
+    // finding if user exists
+    let user = await User.findOne({ email });
+    if (!user) {
+      errorCode = 403;
+      throw new Error("Please try to login with correct credentials");
     }
-}
+
+    // comparing passwords
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    console.log(passwordCompare);
+    if (!passwordCompare) {
+      errorCode = 403;
+      throw new Error("Please try to login with correct credentials");
+    }
+
+    // if passwords get authenticated, creating auth token
+    const data = {
+      user: {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        name: user.name,
+      },
+    };
+    const authToken = jwt.sign(data, secKey);
+
+    // sending response
+    res.json({
+      authToken,
+      success: true,
+      user: data.user,
+      message: "Login Successful",
+    });
+  } catch (err) {
+    // sending response with errorCode 500 and error message if any error raised
+    return res
+      .status(errorCode || 500)
+      .json({
+        success: false,
+        message: "Internal server Error",
+        error: err.message,
+      });
+  }
+};
 
 module.exports = loginUser;
--- server/Routes/profile/getProfile.js
+++ server/Routes/profile/getProfile.js
@@ -1,20 +1,28 @@
-const Profile = require('../../Models/profile');
-
+const Profile = require("../../Models/profile");
 
 const getProfile = async (req, res) => {
-    let errorCode = null;
-    try {
-        let username = req.headers.username;
-        username = username.toLowerCase();
-        const profile = await Profile.findOne({ username });
-        if (!profile) {
-            errorCode = 404;
-            throw new Error("Profile not found")
-        }
-        res.status(200).json({ profile, success: true });
-    } catch (error) {
-        return res.status(errorCode || 500).json({ success: false, message: "Internal server Error", error: error.message })
+  let errorCode = null;
+  try {
+    // extracting username from headers
+    let username = req.headers.username;
+    username = username.toLowerCase();
 
+    // finding profile
+    const profile = await Profile.findOne({ username });
+    if (!profile) {
+      errorCode = 404;
+      throw new Error("Profile not found");
     }
-}
+
+    // sending profile
+    res.status(200).json({ profile, success: true });
+  } catch (error) {
+    // sending errors if any occurs
+    return res.status(errorCode || 500).json({
+      success: false,
+      message: "Internal server Error",
+      error: error.message,
+    });
+  }
+};
 module.exports = getProfile;
